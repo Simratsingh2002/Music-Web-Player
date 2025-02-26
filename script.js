f451401ed196a3a1aa409630401af1ce1ac77aa3
Simrat Singh Bhatia <ssbhatia@upei.ca>
2025-02-26 17:33:14 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+ console.log("Lets start with some JS")
+ async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5501/songs/")  
+  let response= await a.text();
+  let div=document.createElement("div")
+  div.innerHTML=response;
+  let as=div.getElementsByTagName("a")
+  let songs=[]
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+        songs.push(element.href.split("/songs/")[1])
+    }
+
+    
+  }
+  return songs
+ }
+ async function main(){
+    let songs=await getSongs()
+    console.log(songs)
+
+    let songUL=document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        // songUL.innerHTML=songUL+song
+        songUL.innerHTML=songUL.innerHTML+ `<li>${song.replaceAll("%20"," ")}</li>` ;
+    }
+    //Playing the song
+    var audio=new Audio(songs[0]);
+    //audio.play();
+
+    audio.addEventListener("loadedmetadata",()=>{        //loadedmetadata does—it waits for the song’s info before doing anything else!
+        console.log(audio.duration,audio.currentSrc,audio.currentTime)
+    });
+ }
+ main()
